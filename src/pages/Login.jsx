77dfd48b1293a698a51401afae40807e2d7e696a
Juan Dure Faccini <juanchidure@outlook.com>
2022-08-16 22:59:55 -0300

    Delete react-hook-form and move dependencies to devDependencies

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,122 +14,136 @@ import * as yup from "yup";
 import { useFormik } from "formik";
 
 const validationSchema = yup.object({
-  email2b: yup
-    .string("Ingrese su correo electronico")
-    .email("Ingrese un correo electronico valido")
-    .required("El correo electronico es requerido"),
-  password: yup
-    .string("Ingrese su contraseña")
-    .required("La contraseña es requerida"),
+    email2b: yup
+        .string("Ingrese su correo electronico")
+        .email("Ingrese un correo electronico valido")
+        .required("El correo electronico es requerido"),
+    password: yup
+        .string("Ingrese su contraseña")
+        .required("La contraseña es requerida"),
 });
 
 function Login() {
-  const user = useSelector((state) => state.user.user);
-  const navigate = useNavigate();
+    const user = useSelector((state) => state.user.user);
+    const navigate = useNavigate();
 
-  const formik = useFormik({
-    initialValues: {
-      email2b: "",
-      password: "",
-    },
-    validationSchema: validationSchema,
-    onSubmit: async (values) => {
-      setError(false);
-      const loggedIn = await login(values.email2b, values.password);
-      if (!loggedIn) {
-        setError("Email o contraseña incorrecto");
-      }
-    },
-  });
+    const formik = useFormik({
+        initialValues: {
+            email2b: "",
+            password: "",
+        },
+        validationSchema: validationSchema,
+        onSubmit: async (values) => {
+            setError(false);
+            const loggedIn = await login(values.email2b, values.password);
+            if (!loggedIn) {
+                setError("Email o contraseña incorrecto");
+            }
+        },
+    });
 
-  useEffect(() => {
-    if (user) {
-      navigate("/home");
-    }
-  }, [user]);
+    useEffect(() => {
+        if (user) {
+            navigate("/home");
+        }
+    }, [user]);
 
-  const [error, setError] = useState(false);
+    const [error, setError] = useState(false);
 
-  return (
-    <Paper elevation={4} sx={{ overflow: "hidden", maxWidth: 432 }}>
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        sx={{
-          width: "100%",
-          height: 200,
-          bgcolor: (theme) => theme.palette.primary.dark,
-          paddingTop: 2,
-          px: 2,
-        }}
-      >
-        <Typography
-          variant="h4"
-          component="h1"
-          sx={{ color: (theme) => theme.palette.primary.contrastText }}
-        >
-          <img
-            style={{ maxWidth: 320, width: "100%" }}
-            src={Logo}
-            alt="2B Conexión"
-          />
-        </Typography>
-      </Box>
-      <Stack p={4} alignItems="center" spacing={1}>
-        <Typography variant="h5" component="h2" textAlign="center">
-          Acceso colaboradores
-        </Typography>
-
-        <form onSubmit={formik.handleSubmit}>
-          <TextInput
-            id="email2b"
-            name="email2b"
-            label="Email"
-            value={formik.values.email}
-            onChange={(e) => {
-              setError(false);
-              formik.handleChange(e);
-            }}
-            error={formik.touched.email2b && Boolean(formik.errors.email2b)}
-            helperText={formik.touched.email2b && formik.errors.email2b}
-          />
-          <TextInput
-            id="password"
-            name="password"
-            label="Contraseña"
-            type="password"
-            value={formik.values.password}
-            onChange={(e) => {
-              setError(false);
-              formik.handleChange(e);
-            }}
-            error={formik.touched.password && Boolean(formik.errors.password)}
-            helperText={formik.touched.password && formik.errors.password}
-          />
-
-          {error && (
-            <Typography
-              variant="body2"
-              sx={{ color: (theme) => theme.palette.error.main }}
+    return (
+        <Paper elevation={4} sx={{ overflow: "hidden", maxWidth: 432 }}>
+            <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                sx={{
+                    width: "100%",
+                    height: 200,
+                    bgcolor: (theme) => theme.palette.primary.dark,
+                    paddingTop: 2,
+                    px: 2,
+                }}
             >
-              {error}
-            </Typography>
-          )}
-          <Button
-            fullWidth
-            variant="contained"
-            type="submit"
-            sx={{ my: 2, py: 1, fontSize: "1.2em" }}
-          >
-            Ingresar
-          </Button>
-        </form>
-        <Link variant="body2">Recuperar contraseña</Link>
-        <Link variant="body2">Solicitar alta de usuario</Link>
-      </Stack>
-    </Paper>
-  );
+                <Typography
+                    variant="h4"
+                    component="h1"
+                    sx={{
+                        color: (theme) => theme.palette.primary.contrastText,
+                    }}
+                >
+                    <img
+                        style={{ maxWidth: 320, width: "100%" }}
+                        src={Logo}
+                        alt="2B Conexión"
+                    />
+                </Typography>
+            </Box>
+            <Stack p={4} alignItems="center" spacing={1}>
+                <Typography variant="h5" component="h2" textAlign="center">
+                    Acceso colaboradores
+                </Typography>
+
+                <form onSubmit={formik.handleSubmit}>
+                    <TextInput
+                        id="email2b"
+                        name="email2b"
+                        label="Email"
+                        value={formik.values.email}
+                        onChange={(e) => {
+                            setError(false);
+                            formik.handleChange(e);
+                        }}
+                        error={
+                            (formik.touched.email2b &&
+                                Boolean(formik.errors.email2b)) ||
+                            error
+                        }
+                        helperText={
+                            formik.touched.email2b && formik.errors.email2b
+                        }
+                    />
+                    <TextInput
+                        id="password"
+                        name="password"
+                        label="Contraseña"
+                        type="password"
+                        value={formik.values.password}
+                        onChange={(e) => {
+                            setError(false);
+                            formik.handleChange(e);
+                        }}
+                        error={
+                            (formik.touched.password &&
+                                Boolean(formik.errors.password)) ||
+                            error
+                        }
+                        helperText={
+                            formik.touched.password && formik.errors.password
+                        }
+                    />
+
+                    {error && (
+                        <Typography
+                            variant="body2"
+                            sx={{ color: (theme) => theme.palette.error.main }}
+                        >
+                            {error}
+                        </Typography>
+                    )}
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        type="submit"
+                        sx={{ my: 2, py: 1, fontSize: "1.2em" }}
+                    >
+                        Ingresar
+                    </Button>
+                </form>
+                <Link variant="body2">Recuperar contraseña</Link>
+                <Link variant="body2">Solicitar alta de usuario</Link>
+            </Stack>
+        </Paper>
+    );
 }
 
 export default Login;
