3262a52fa7c2d858ecb6da6152aa78122df5bb00
Juan Dure Faccini <juanchidure@outlook.com>
2022-08-17 16:04:20 -0300

    Create base modal

--- src/components/form/TextInput.jsx
+++ src/components/form/TextInput.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import { TextField } from "@mui/material";
 
 function FormText(props) {
-  return (
-    <TextField
-      id={props.id}
-      name={props.name}
-      label={props.label}
-      value={props.value}
-      onChange={props.onChange}
-      error={props.error}
-      disabled={!!props.disabled}
-      type={props.type}
-      helperText={props.helperText}
-      variant="outlined"
-      fullWidth
-      margin="normal"
-    />
-  );
+    return (
+        <TextField
+            id={props.id}
+            name={props.name}
+            label={props.label}
+            value={props.value}
+            onChange={props.onChange}
+            error={Boolean(props.error)}
+            disabled={!!props.disabled}
+            type={props.type}
+            helperText={props.helperText}
+            variant="outlined"
+            fullWidth
+            margin="normal"
+        />
+    );
 }
 
 export default FormText;
