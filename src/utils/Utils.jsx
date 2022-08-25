7fda1535bbfb44a689c2fe0c4c3dac9a14cee26b
Juan Dure Faccini <juanchidure@outlook.com>
2022-08-25 13:05:41 -0300

    [WIP] Component validation

--- src/utils/Utils.jsx
+++ src/utils/Utils.jsx
@@ -2,10 +2,10 @@ import ClientDataStepBundle from "../components/newOperation/ClientDataStep";
 import FirstStepBundle from "../components/newOperation/FirstStep";
 
 const StepArray = [FirstStepBundle, ClientDataStepBundle];
-const isValid = (value) => {
-    console.log("value: ", value);
-    value.ValidationSchema && value.Label && value.ReactComponent;
-};
-const res = StepArray.every(isValid);
-console.log("res ", res);
+StepArray.forEach((value) => {
+    const isStepValid =
+        value.ValidationSchema && value.Label && value.ReactComponent;
+    if (!isStepValid) throw "Invalid Step";
+});
+
 export default StepArray;
