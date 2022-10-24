10654daecc85489319f9761d27d3c55886288172
Juan Dure Faccini <juanchidure@outlook.com>
2022-10-24 11:03:05 -0300

    Fix bug

--- src/components/form/AdditionalsFieldArray.jsx
+++ src/components/form/AdditionalsFieldArray.jsx
@@ -4,19 +4,16 @@ import { Button, Box, MenuItem, Select, InputLabel, FormControl, IconButton } fr
 import FormSelect from "./FormSelect";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
+import AdditionalSelect from "./AdditionalSelect";
 
 export default function AdditionalsFieldArray({ disabled, name, avaibleAdditionals }) {
     const [field] = useField(name);
-    const [additionalType, setAdditionalType] = useState(true);
 
     const lastIsEmpty = () => {
         const lastValue = field.value[field.value.length - 1];
         return lastValue == "";
     };
 
-    const handleTypeChange = (event) => {
-        setAdditionalType(event.target.value);
-    };
     return (
         <>
             <FieldArray name="additionals">
@@ -24,42 +21,9 @@ export default function AdditionalsFieldArray({ disabled, name, avaibleAdditiona
                     <Box sx={{ width: "100%", mt: 2 }}>
                         {field.value.map((_, index) => (
                             <Box key={index} sx={{ mt: 2, display: "flex", width: "100%" }}>
-                                <Box
-                                    sx={{
-                                        width: "30%",
-                                    }}
-                                >
-                                    <FormControl fullWidth>
-                                        <InputLabel id="select-label">Tipo de adicional</InputLabel>
-                                        <Select labelId="select-label" id="algo" label="Tipo de adicional" value={additionalType} onChange={handleTypeChange}>
-                                            <MenuItem key={1} value={true}>
-                                                Popular
-                                            </MenuItem>
-                                            <MenuItem key={2} value={false}>
-                                                No popular
-                                            </MenuItem>
-                                        </Select>
-                                    </FormControl>
+                                <Box sx={{ width: "90%" }}>
+                                    <AdditionalSelect name={name} avaibleAdditionals={avaibleAdditionals} index={index} disabled={disabled} />
                                 </Box>
-                                <Box
-                                    sx={{
-                                        pl: 1,
-                                        width: "60%",
-                                    }}
-                                >
-                                    <FormSelect name={`${name}[${index}]`} label={"Adicional " + (index + 1)} disabled={disabled || additionalType == undefined}>
-                                        {avaibleAdditionals.map((a) => {
-                                            return a.options
-                                                .filter((o) => o.popular == additionalType)
-                                                .map((o) => (
-                                                    <MenuItem key={o.id} value={o}>
-                                                        <strong>{a.name}</strong>: {o.name}
-                                                    </MenuItem>
-                                                ));
-                                        })}
-                                    </FormSelect>
-                                </Box>
-
                                 <Box sx={{ flexGrow: 0, width: "10%", flexShrink: 0, display: "flex", justifyContent: "center" }}>
                                     <IconButton sx={{}} onClick={() => remove(index)} variant="contained">
                                         <DeleteIcon color="primary" />
