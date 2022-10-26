cdb452a4de2711969541eb2d685ccf15836af213
Juan Dure Faccini <juanchidure@outlook.com>
2022-10-26 10:50:14 -0300

    Add role check before displaying columns

--- src/components/Operations.jsx
+++ src/components/Operations.jsx
@@ -1,41 +1,37 @@
 import * as React from "react";
-import { Box, Button, IconButton } from "@mui/material";
+import { Box } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { data } from "../mock/OperationsMock";
-import moment from "moment";
+import { getOperationTableColumnsByRole } from "../utils/RolesUtils";
+import { IconButton } from "@mui/material";
 import ReadMoreIcon from "@mui/icons-material/ReadMore";
 
-const columns = [
-    { flex: 1, field: "creationDate", headerName: "Fecha", width: 220, valueGetter: (params) => moment(params?.row?.creationDate).format("DD/MM/YYYY") },
-    { flex: 1, field: "operationCode", headerName: "Codigo Operacion", width: 150 },
-    { flex: 1, field: "status", headerName: "Estado", width: 150 },
-    { flex: 1, field: "processorId", headerName: "Tramitadora", width: 110 },
-    { flex: 1, field: "customer[lastname]", headerName: "Apellidos", width: 110, valueGetter: (params) => params?.row?.customer?.lastname },
-    {
-        field: "details",
-        headerName: "Detalles",
-        width: 110,
-        renderCell: (cellValues) => {
-            return (
-                <IconButton
-                    variant="contained"
-                    color="primary"
-                    onClick={(event) => {
-                        handleClick(event, cellValues);
-                    }}
-                >
-                    <ReadMoreIcon />
-                </IconButton>
-            );
-        },
-    },
-];
-
-const rows = [data];
-
 export default function Operations() {
+    const columns = getOperationTableColumnsByRole();
+    const rows = [data];
+
+    if (!columns) return <></>;
+
+    function handleClick(event, cellValues) {
+        console.log("CLICK");
+    }
+
+    columns.find((col) => col.headerName === "Detalles").renderCell = (cellValues) => {
+        return (
+            <IconButton
+                variant="contained"
+                color="primary"
+                onClick={(event) => {
+                    handleClick(event, cellValues);
+                }}
+            >
+                <ReadMoreIcon />
+            </IconButton>
+        );
+    };
+
     return (
-        <Box sx={{ height: 400, width: "100%", background: "white" }}>
+        <Box sx={{ height: 400, width: "100%", background: "white", p: 2 }}>
             <DataGrid rows={rows} columns={columns} pageSize={5} rowsPerPageOptions={[5]} disableSelectionOnClick experimentalFeatures={{ newEditingApi: true }} />
         </Box>
     );
---
+++ src/utils/RolesUtils.js
@@ -0,0 +1,52 @@
+import { store } from "../state/store";
+import moment from "moment";
+
+const colaboratorColumns = [
+    { headerAlign: "center", align: "center", flex: 1, field: "creationDate", headerName: "Fecha", valueGetter: (params) => moment(params?.row?.creationDate).format("DD/MM/YYYY") },
+    { headerAlign: "center", align: "center", flex: 1, field: "operationCode", headerName: "Codigo Operacion" },
+    { headerAlign: "center", align: "center", flex: 1, field: "status", headerName: "Estado" },
+    { headerAlign: "center", align: "center", flex: 1, field: "processorId", headerName: "Tramitadora" },
+    { headerAlign: "center", align: "center", flex: 1, field: "customer[lastname]", headerName: "Apellidos", valueGetter: (params) => params?.row?.customer?.lastname },
+    { headerAlign: "center", align: "center", field: "details", headerName: "Detalles", width: 110 },
+];
+
+const adminColumns = [
+    { headerAlign: "center", align: "center", flex: 1, field: "creationDate", headerName: "Fecha", valueGetter: (params) => moment(params?.row?.creationDate).format("DD/MM/YYYY") },
+    { headerAlign: "center", align: "center", flex: 1, field: "operationCode", headerName: "Codigo Operacion" },
+    { headerAlign: "center", align: "center", flex: 1, field: "status", headerName: "Estado" },
+    { headerAlign: "center", align: "center", flex: 1, field: "channel", headerName: "Canal Tramitacion" },
+    { headerAlign: "center", align: "center", flex: 1, field: "colaboratorCode", headerName: "Codigo colaborador" },
+    { headerAlign: "center", align: "center", flex: 1, field: "customer[lastname]", headerName: "Apellidos", valueGetter: (params) => params?.row?.customer?.lastname },
+    { headerAlign: "center", align: "center", field: "details", headerName: "Detalles", width: 110 },
+];
+
+export function isAdmin() {
+    return store.getState().session.user.roles.some((r) => r === "ADMIN");
+}
+
+export function getRoles() {
+    return store.getState().session.user.roles;
+}
+
+function hasAnyRole(roles) {
+    const currentUserRoles = getRoles();
+    return currentUserRoles.some((r) => roles.includes(r));
+}
+
+function hasRole(role) {
+    const currentUserRoles = getRoles();
+    return currentUserRoles.some((r) => r === role);
+}
+
+export function getOperationTableColumnsByRole() {
+    let roles = ["ADMIN", "PROCESSOR"];
+    if (hasAnyRole(roles)) {
+        return adminColumns;
+    }
+
+    if (hasRole("USER")) {
+        return colaboratorColumns;
+    }
+
+    return [];
+}
