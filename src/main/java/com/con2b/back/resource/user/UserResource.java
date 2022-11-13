fef6914876bcf8a7a71cde07c376b917c59bdde1
Rocio <giannaccini32@gmail.com>
2022-11-12 21:14:46 -0300

    Code fixes

--- src/main/java/com/con2b/back/resource/user/UserResource.java
+++ src/main/java/com/con2b/back/resource/user/UserResource.java
@@ -2,34 +2,24 @@ package com.con2b.back.resource.user;
 
 import com.con2b.back.dto.GenericResponseDTO;
 import com.con2b.back.service.user.UserService;
-import com.con2b.back.model.user.User2b;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/users")
 @PreAuthorize("isAuthenticated()")
 public class UserResource {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> getUsers() {
-        return ResponseEntity.ok().body(new GenericResponseDTO(userService.getUsers()));
-    }
-
-    @GetMapping("/{userCode}")
+    @GetMapping("")
     @PreAuthorize("hasRole('ADMIN') || hasRole('PROCESSOR')")
-    public ResponseEntity<?> getUsersByUserCode(@PathVariable String userCode){
+    public ResponseEntity<?> getUsersByUserCode(@RequestParam(defaultValue = "") @DefaultValue() String userCode ){
         return ResponseEntity.ok().body(new GenericResponseDTO(true, userService.getUsersByUserCode(userCode)));
     }
 
