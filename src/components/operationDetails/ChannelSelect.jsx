0463e902b780b4b2d33cd2e0fd1852b6b08d70f4
Juan <juanchidure@outlook.com>
2022-12-08 16:45:17 -0300

    Fix Operation Title Edit Permissions

--- src/components/operationDetails/ChannelSelect.jsx
+++ src/components/operationDetails/ChannelSelect.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { MenuItem, Typography, Box } from "@mui/material";
 import { EditableDetailField } from "../EditableDetailField";
+import { useSelector } from "react-redux";
 
 export function ChannelSelect({ channel, operationStatus }) {
     let channelName = "";
     const permissionNeeded = "CHANNEL";
+    const editPermissions = useSelector((state) => state.operations.permissions);
+    const editPermission = editPermissions[operationStatus];
+    const canEdit = editPermission.includes(permissionNeeded);
 
     if (channel !== null) channelName = channel;
 
@@ -13,19 +17,28 @@ export function ChannelSelect({ channel, operationStatus }) {
             <Typography variant="subtitle2" sx={{ mr: 1, alignItems: "center" }}>
                 Canal
             </Typography>
-            <EditableDetailField value={channelName} name={"selectOptional"} column={permissionNeeded} operationStatus={operationStatus} type="select">
-                <MenuItem value={""}>-</MenuItem>
-                <MenuItem value={"AR"}>AR</MenuItem>
-                <MenuItem value={"FM"}>FM</MenuItem>
-                <MenuItem value={"IR"}>IR</MenuItem>
-                <MenuItem value={"NE"}>NE</MenuItem>
-                <MenuItem value={"OT_LAB"}>OT_LAB</MenuItem>
-                <MenuItem value={"OT_CTG"}>OT_CTG</MenuItem>
-                <MenuItem value={"TF_MUR"}>TF_MUR</MenuItem>
-                <MenuItem value={"SC"}>SC</MenuItem>
-                <MenuItem value={"TOT"}>TOT</MenuItem>
-                <MenuItem value={"TPHS"}>TPHS</MenuItem>
-            </EditableDetailField>
+            {
+                canEdit
+                    ? (
+                        <EditableDetailField value={channelName} name={"selectOptional"} column={permissionNeeded} operationStatus={operationStatus} type="select">
+                            <MenuItem value={""}>-</MenuItem>
+                            <MenuItem value={"AR"}>AR</MenuItem>
+                            <MenuItem value={"FM"}>FM</MenuItem>
+                            <MenuItem value={"IR"}>IR</MenuItem>
+                            <MenuItem value={"NE"}>NE</MenuItem>
+                            <MenuItem value={"OT_LAB"}>OT_LAB</MenuItem>
+                            <MenuItem value={"OT_CTG"}>OT_CTG</MenuItem>
+                            <MenuItem value={"TF_MUR"}>TF_MUR</MenuItem>
+                            <MenuItem value={"SC"}>SC</MenuItem>
+                            <MenuItem value={"TOT"}>TOT</MenuItem>
+                            <MenuItem value={"TPHS"}>TPHS</MenuItem>
+                        </EditableDetailField>
+                    )
+                    : <Typography variant="subtitle2" sx={{ mr: 1, alignItems: "center" }}>
+                        {channelName ? channelName : "-"}
+                    </Typography>
+            }
+
         </Box>
     );
 }
--- src/components/operationDetails/ProcessorSelect.jsx
+++ src/components/operationDetails/ProcessorSelect.jsx
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from "react";
 import { Typography, Box, MenuItem } from "@mui/material";
 import { fetchProcessors } from "../../services/CollaboratorService";
 import { EditableDetailField } from "../EditableDetailField";
+import { useSelector } from "react-redux";
+
 
 export function ProcessorSelect({ processor, operationStatus }) {
     const [processors, setProcessors] = useState([]);
-
     const column = "PROCESSOR";
-
-    useEffect(() => {
-        fetchOptions();
-    }, []);
+    const editPermissions = useSelector((state) => state.operations.permissions);
+    const editPermission = editPermissions[operationStatus];
+    const canEdit = editPermission.includes(column);
 
     const fetchOptions = async(value) => {
         const res = await fetchProcessors(value);
@@ -20,14 +20,25 @@ export function ProcessorSelect({ processor, operationStatus }) {
         setProcessors(mappedOptions);
     };
 
+    useEffect(() => {
+        if (canEdit) fetchOptions();
+    }, []);
+
+
     return (
         <Box sx={{ display: "flex", alignItems: "center" }}>
             <Typography variant="subtitle2" sx={{ mr: 1, alignItems: "center" }}>
                 Tramitadora
             </Typography>
-            <EditableDetailField value={processor?.userCode} name={"processor"} operationStatus={operationStatus} column={column} type={"select"} small>
-                {processors.length > 0 && processors.map((processor, index) => <MenuItem value={processor.userCode} key={index}>{processor.userCode}</MenuItem>)}
-            </EditableDetailField>
+            {
+                canEdit
+                    ? (<EditableDetailField value={processor?.userCode} name={"processor"} operationStatus={operationStatus} column={column} type={"select"} small>
+                        {processors.length > 0 && processors.map((processor, index) => <MenuItem value={processor.userCode} key={index}>{processor.userCode}</MenuItem>)}
+                    </EditableDetailField>)
+                    : <Typography variant="subtitle2" sx={{ mr: 1, alignItems: "center" }}>
+                        {processor?.userCode ? processor.userCode : "-"}
+                    </Typography>
+            }
         </Box>
     );
 }
